535f5bb4d63d32b57c7626506f9a6ea4a968a76a
fiqrioemry <foemry@gmail.com>
2025-06-23 09:17:43 +0700

    feat(add):configure app entry poin and server

---
+++ server/src/app.ts
@@ -0,0 +1,32 @@
+import dotenv from "dotenv";
+import express, { Application } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import errorHandler from "./errors/errorHandler";
+import initRoutes from "./routes/route";
+import limiter from "./middlewares/limiter.middleware";
+
+dotenv.config();
+
+const app: Application = express();
+
+// Middleware
+app.use(limiter);
+app.use(cookieParser());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+// Routes
+initRoutes(app);
+
+// Error Handler
+app.use(errorHandler);
+
+export default app;
